d7531174e7223d92ef431d440b063d67bf523e8d
Douglas Mateus Teixeira <douglastx007@gmail.com>
2022-07-22 13:08:20 -0300

    Editando o Login Form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,13 +4,15 @@ import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 import { UserContext } from '../../UseContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
 
 
 const LoginForm = () => {
     const username = useForm();
     const password = useForm();
 
-    const {userLogin} = React.useContext(UserContext);
+    const {userLogin, error, loading} = React.useContext(UserContext);
 
 
  async function handleSubmit(event) { 
@@ -22,16 +24,20 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-     <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+     <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
 
         <Input label="Usuário" type="text" name="username" {...username}/>
         <Input label="Senha" type="password" name="password" {...password}/>
-
-       <Button>Entrar</Button>
+          {loading ? <Button disabled>Carregando...</Button> : <Button>Entrar</Button>}
+          <Error error={error}/>
       </form>
-
+      <Link className={styles.perdeu} to="/login/perdeu">Perdeu a Senha?</Link>
+      <div className={styles.cadastro}>
+         <h2 className={styles.subtitle}>Cadastre-se</h2>
+         <p>Ainda não possui conta? Cadastre-se no site.</p>
+      </div>
       <Link to="/login/criar">Cadastro</Link>
     </section>
   )
